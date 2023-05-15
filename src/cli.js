8cf6ec9cc4e73e0a41737946ad44280909810e61
Ivan Babkov <babkov1738@gmail.com>
2023-05-15 12:28:21 +0300

    fix 3rd step

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import realineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 const sayGreetings = () =>{
     console.log("Welcome to the Brain Games!");
-    const name = realineSync.questions('May I have your name?');
+    const name = readlineSync.question('May I have your name?');
     console.log(`Hello, ${name}!`);
 };
 export default sayGreetings
