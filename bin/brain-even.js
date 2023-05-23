fff714046e74a2e62e48ca24c5e996dcdedf138f
Ivan Babkov <babkov1738@gmail.com>
2023-05-23 21:46:50 +0300

    finished 5th step

---
+++ bin/brain-even.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import isEven from '../src/games/isEven.js';
+isEven();
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import  sayGreetings from './cli.js';
+import { getYourAnswer } from './math.js';
+
+const game = (task, getQuestionAndAnswer) => {
+  const name = sayGreetings();
+  console.log(task);
+  const roundsCount = 3;
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, answer] = getQuestionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = getYourAnswer();
+    if (answer === userAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default game;
